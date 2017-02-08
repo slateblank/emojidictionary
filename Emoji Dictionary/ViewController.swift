2db6c561408c54df8ba693a297722402f9296bbc
Matt Harp <mattharp@Matts-MacBook-Pro.local>
2017-02-08 13:41:20 -0600

    Added in Table view

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource ,UITableViewDelegate {
 
+    @IBOutlet weak var maintable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        maintable.dataSource = self  //says where data comes from
+        maintable.delegate = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+            cell.textLabel?.text  = "HELLO THERE"
+        return cell
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
